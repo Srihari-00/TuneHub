979061084d991de2bbcb53a49e65b8498361f2bd
Srihari-00 <sriharig2000@gmail.com>
2024-02-05 20:10:39 +0530

    added controller

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.entity.LoginData;
 import com.example.demo.entity.Song;
 import com.example.demo.entity.Users;
@@ -16,7 +19,8 @@ import jakarta.servlet.http.HttpSession;
 
 
 
-@Controller
+@CrossOrigin("*")
+@RestController
 public class UsersController 
 {
 	@Autowired
